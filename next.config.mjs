b147e9ff90d3f7f4c89c5dd3736a1411bd70a1cb
Jay N T <jp.tollie@gmail.com>
2024-12-23 14:03:42 +0800

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/earn-wmtx',
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
+
