a0864ad6e0e5107f14142e79c1c9a810e010a63f
Jay N T <jp.tollie@gmail.com>
2025-01-02 13:09:04 +0800

    updated structure

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://wmtx.worldmobile.club',
-  generateRobotsTxt: true,
-  outDir: 'out',
-}
