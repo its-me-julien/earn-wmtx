a7aaf1f5238b8ca47a2355d2c7877418ee35921e
Jay N T <jp.tollie@gmail.com>
2025-01-02 14:02:05 +0800

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://wmtx.worldmobile.club', // Your website's URL
+    generateRobotsTxt: true,
+    generateRobotsTxt: false, // Robots.txt is already generated
+    sitemapSize: 5000, // Adjust if you have many pages
+    changefreq: 'daily', // Optional: default change frequency
+    priority: 0.7, // Optional: default priority
+  };
+  
